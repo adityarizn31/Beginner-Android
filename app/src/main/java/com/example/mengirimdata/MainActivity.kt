5c5e6590c8151b4c05072f61a41b639afefc190e
[adityarn] <[adityarizkiawann@gmail.com]>
2025-02-18 15:42:37 +0700

    Belajar Mengirim Data

---
+++ app/src/main/java/com/example/mengirimdata/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.mengirimdata
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnMoveWithDataActivity : Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.btn_move_activity_data -> {
+                val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Aditya")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 23)
+                startActivity(moveWithDataIntent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mengirimdata/MoveWithDataActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mengirimdata
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MoveWithDataActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_AGE = "extra_age"
+        const val EXTRA_NAME = "extra_name"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_move_with_data)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val tvDataReceived : TextView = findViewById(R.id.tvDataReceived)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val age = intent.getIntExtra(EXTRA_AGE, 1)
+
+        val text = "Nama : $name \n Umur : $age "
+        tvDataReceived.text = text
+    }
+}
