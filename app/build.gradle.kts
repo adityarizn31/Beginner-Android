98ac99a1e0ddc3f012f2a789e186f772a3bb0bcf
[adityarn] <[adityarizkiawann@gmail.com]>
2025-03-07 00:48:33 +0700

    Learning Theme & Style

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.constraintlayout"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.constraintlayout"
